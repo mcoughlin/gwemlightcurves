3de04545bb43e00f62ff7349843c4cb9fa0dcbca
Michael Coughlin <michael.coughlin@ligo.org>
2019-01-05 18:17:38 -0800

    update downloads

--- gwemlightcurves/ztf_utils.py
+++ gwemlightcurves/ztf_utils.py
@@ -1,5 +1,5 @@
 
-import os, sys
+import os, sys, json
 from matplotlib import pyplot as plt
 import requests
 import numpy as np
@@ -11,7 +11,8 @@ except:
     print('Please install penquins')
     pass
 
-def get_ztf(filename, name, username, password, filetype = "default"):
+def get_ztf(filename, name, username, password,
+            filetype = "default", z=0.0, zerr=0.0001, SN_Type="Ia"):
 
     k = Kowalski(username=username, password=password, verbose=True)
 
@@ -22,6 +23,8 @@ def get_ztf(filename, name, username, password, filetype = "default"):
     if len(r['result_data']['query_result']) >0:
         candidate = r['result_data']['query_result'][0]
         prevcandidates= r['result_data']['query_result'][0]['prv_candidates']
+
+        print(candidate,prevcandidates)
 
         jd = [candidate['candidate']['jd']]
         mag = [candidate['candidate']['magpsf']]
@@ -56,13 +59,65 @@ def get_ztf(filename, name, username, password, filetype = "default"):
             t = Time(jd[ii], format='jd').isot
             fid.write('%s %s %.5f %.5f\n'%(t,filtname[ii],mag[ii],magerr[ii]))
     elif filetype == "snmachine":
-        z, zerr = 0.1, 0.01
         fid.write('HOST_GALAXY_PHOTO-Z:   %.4f  +- %.4f\n'%(z,zerr))
-        fid.write('SIM_COMMENT:  SN Type = II\n')
+        fid.write('SIM_COMMENT:  SN Type = %s\n'%SN_Type)
         for ii in idx:
             t = Time(jd[ii], format='jd').mjd
-            flux = 10**(mag[ii]/(-2.5))
+            flux = 1e9 * 10**(mag[ii]/(-2.5))
             fluxerr = magerr[ii]*flux
             fid.write('OBS: %.3f %s NULL %.3e %.3e %.2f %.5f %.5f\n'%(t,filtname[ii],flux,fluxerr,flux/fluxerr,mag[ii],magerr[ii]))
     fid.close()
 
+def get_ztf_lc(filename, name, username, password,
+               filetype = "default", z=0.0, zerr=0.0001, SN_Type="Ia"):
+
+    r = requests.post('http://skipper.caltech.edu:8080/cgi-bin/growth/print_lc.cgi', auth=(username, password), data={'name' : name})
+    lines = r.text.replace(" ","").replace("\n","").replace('"','').split("issub")[-1].split("<br>")
+    jd, filtname, mag, magerr = [], [], [], []
+    
+    for line in lines:
+        lineSplit = line.split(",")
+        lineSplit = list(filter(None,lineSplit))
+        if not lineSplit: continue
+        if len(lineSplit) > 11:
+            lineSplit = lineSplit[:11]
+
+        if len(lineSplit) == 10:
+            date,jdobs,filt,magpsf,sigmamagpsf,limmag,instrument,reducedby,refsys,issub = lineSplit
+        else:
+            date,jdobs,filt,absmag,magpsf,sigmamagpsf,limmag,instrument,reducedby,refsys,issub = lineSplit
+
+        jd.append(float(jdobs))
+        filtname.append(filt)
+        if float(magpsf) < -100:
+            magpsf = "99.0"
+        if float(limmag) < -100:
+            limmag = "99.0"
+
+        if np.isclose(float(magpsf),99.0):
+            mag.append(float(limmag))
+            magerr.append(np.inf)
+        else:
+            mag.append(float(magpsf))
+            magerr.append(float(sigmamagpsf))
+    idx = np.argsort(jd)
+
+    fid = open(filename,'w')
+    if filetype == "default":
+        for ii in idx:
+            t = Time(jd[ii], format='jd').isot
+            fid.write('%s %s %.5f %.5f\n'%(t,filtname[ii],mag[ii],magerr[ii]))
+    elif filetype == "snmachine":
+        fid.write('HOST_GALAXY_PHOTO-Z:   %.4f  +- %.4f\n'%(z,zerr))
+        fid.write('SIM_COMMENT:  SN Type = %s\n'%SN_Type)
+        for ii in idx:
+            t = Time(jd[ii], format='jd').mjd
+            flux = 1e9 * 10**(mag[ii]/(-2.5))
+            fluxerr = magerr[ii]*flux
+            if np.isclose(fluxerr,0.0,atol=1e-12):
+                snr = np.inf
+            else:
+                snr = flux/fluxerr
+            if not np.isfinite(magerr[ii]): continue
+            fid.write('OBS: %.3f %s NULL %.3e %.3e %.2f %.5f %.5f\n'%(t,filtname[ii],flux,fluxerr,snr,mag[ii],magerr[ii]))
+    fid.close()
