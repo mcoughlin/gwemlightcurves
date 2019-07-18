630ceaf13fb458050137b57088fc3ddd6a4a071e
Michael Coughlin <michael.coughlin@ligo.org>
2019-07-17 17:51:09 -0700

    Include the GW170817 measurements

--- bin/KNe_stdcandle.py
+++ bin/KNe_stdcandle.py
@@ -31,6 +31,8 @@ import scipy.stats as ss
 import pymultinest
 import os
 
+from gwemlightcurves.KNModels import KNTable
+
 def parse_commandline():
     """
     Parse the options given on the command-line.
@@ -40,6 +42,10 @@ def parse_commandline():
     parser.add_option("-o","--outputDir",default="../output")
     parser.add_option("-p","--plotDir",default="../plots")
     parser.add_option("-d","--dataDir",default="../data")
+    parser.add_option("--nsamples",default=-1,type=int)
+
+    parser.add_option("--multinest_samples", default="../plots/gws/Ka2017_FixZPT0/u_g_r_i_z_y_J_H_K/0_14/ejecta/GW170817/1.00/2-post_equal_weights.dat")
+    parser.add_option("-m","--model",default="Ka2017", help="Ka2017,Ka2017x2")
 
     opts, args = parser.parse_args()
 
@@ -82,6 +88,14 @@ baseplotDir = os.path.join(opts.plotDir,'standard_candles')
 if not os.path.isdir(baseplotDir):
     os.makedirs(baseplotDir)
 
+plotDir = baseplotDir
+if not os.path.isdir(plotDir):
+    os.makedirs(plotDir)
+
+ModelPath = '%s/svdmodels'%(opts.outputDir)
+if not os.path.isdir(ModelPath):
+    os.makedirs(ModelPath)
+
 filename1 = os.path.join(opts.dataDir, 'standard_candles', 'magcolor.dat')
 filename2 = os.path.join(opts.dataDir, 'standard_candles', 'magdmdt.dat')
 
@@ -99,10 +113,6 @@ max_iter = 0
 title_fontsize = 26
 label_fontsize = 30
 
-plotDir = baseplotDir
-if not os.path.isdir(plotDir):
-    os.makedirs(plotDir)
-
 parameters = ["K","alpha","beta","gamma","delta","zeta","sigma"]
 labels = [r'K', r'$\alpha$', r'$\beta$', r'$\gamma$', r"$\delta$",r"$\zeta$",r'$\sigma$']
 n_params = len(parameters)
@@ -112,7 +122,7 @@ pymultinest.run(myloglike, myprior, n_params, importance_nested_sampling = False
 multifile = "%s/2-post_equal_weights.dat"%plotDir
 data = np.loadtxt(multifile)
 
-K, alpha, beta, gamma, delta, sigma, zeta, loglikelihood = data[:,0], data[:,1], data[:,2], data[:,3], data[:,4], data[:,5], data[:,6], data[:,7]
+K, alpha, beta, gamma, delta, zeta, sigma, loglikelihood = data[:,0], data[:,1], data[:,2], data[:,3], data[:,4], data[:,5], data[:,6], data[:,7]
 idx = np.argmax(loglikelihood)
 K_best, alpha_best, beta_best, gamma_best, delta_best, zeta_best, sigma_best = data[idx,0:-1]
 
@@ -143,7 +153,7 @@ for ii in range(len(vej_unique)):
         if not ii == len(vej_unique) - 1:
             plt.setp(ax.get_xticklabels(), visible=False)
         else:
-            plt.xlabel('$M_{\mathrm{ej}} = 10^{%.0f}$' % Xlan_unique[jj])
+            plt.xlabel('$X_{\mathrm{lan}} = 10^{%.0f}$' % Xlan_unique[jj])
         if not jj == 0:
             plt.setp(ax.get_yticklabels(), visible=False)
         else:
@@ -181,3 +191,73 @@ plotName = os.path.join(plotDir,'fit.pdf')
 plt.savefig(plotName)
 plt.close()
 
+samples = KNTable.read_multinest_samples(opts.multinest_samples, opts.model)
+if opts.nsamples > 0:
+    samples = samples.downsample(Nsamples=opts.nsamples)
+# These are the default values supplied with respect to generating lightcurves
+tini = 0.1
+tmax = 14.0
+dt = 0.1
+#add default values from above to table
+samples['tini'] = tini
+samples['tmax'] = tmax
+samples['dt'] = dt
+
+kwargs = {'SaveModel':False,'LoadModel':True,'ModelPath':ModelPath}
+kwargs["doAB"] = True
+kwargs["doSpec"] = False
+
+# Create dict of tables for the various models, calculating mass ejecta velocity of ejecta and the lightcurve from the model
+model_table = KNTable.model(opts.model, samples, **kwargs)
+
+K_mean, K_std = np.mean(K), np.std(K)
+alpha_mean, alpha_std = np.mean(alpha), np.std(alpha)
+beta_mean, beta_std = np.mean(beta), np.std(beta)
+gamma_mean, gamma_std = np.mean(gamma), np.std(gamma)
+delta_mean, delta_std = np.mean(delta), np.std(delta)
+zeta_mean, zeta_std = np.mean(zeta), np.std(zeta)
+sigma_mean, sigma_std = np.mean(sigma), np.std(sigma)
+
+N = 10000
+idx = np.random.randint(0, high=len(samples), size=N)
+
+K = np.random.normal(loc=K_mean, scale=K_std, size=N)
+alpha = np.random.normal(loc=alpha_mean, scale=alpha_std, size=N)
+beta = np.random.normal(loc=beta_mean, scale=beta_std, size=N)
+gamma = np.random.normal(loc=gamma_mean, scale=gamma_std, size=N)
+delta = np.random.normal(loc=delta_mean, scale=delta_std, size=N)
+zeta = np.random.normal(loc=zeta_mean, scale=zeta_std, size=N)
+sigma = np.random.normal(loc=0.0, scale=sigma_mean, size=N)
+
+mus = []
+for ii in range(N):
+    row = samples[idx[ii]]
+    model = model_table[idx[ii]]
+    mag = model['mag']
+    t = model['t']
+    Kband = mag[-1]
+    iband = mag[-6]
+
+    jj = np.argmin(Kband)
+    jj7 = np.argmin(np.abs(t-(t[jj]+7.0)))
+    M = Kband[jj] + 5*(np.log10(40.0*1e6) - 1)
+    col = Kband[jj] - iband[jj]
+    m7 = Kband[jj7]-Kband[jj]
+
+    mej, vej, Xlan = row['mej'], row['vej'], row['Xlan']
+    mu = -( -M + K[ii] + m7*alpha[ii] + col*beta[ii] + np.log10(mej)*gamma[ii] + vej*delta[ii] + np.log10(Xlan)*zeta[ii] + sigma[ii])
+    mus.append(mu)
+mus = np.array(mus)
+dist = 10**((mus/5.0) + 1.0) / 1e6
+
+plt.figure(figsize=(10,12))
+n, bins, patches = plt.hist(dist, 50, normed=1, facecolor='green', alpha=0.75)
+plt.xlabel('Distance [Mpc]')
+plt.ylabel('Probability')
+plt.grid(True)
+plt.show()
+plotName = os.path.join(plotDir,'dist.pdf')
+plt.savefig(plotName)
+plt.close()
+
+print('Distance: %.1f +- %.1f' % (np.mean(dist), np.std(dist)))
