e42a3d5355848c65cdbe85ab2424c90f0cc0329e
Andrew Michael Toivonen <andrew.toivonen@ldas-pcdev1.ligo.caltech.edu>
2022-01-24 13:08:39 -0800

    fixing path for tests

--- gwemlightcurves/embright_gwlc/mass_grid_gp10.py
+++ gwemlightcurves/embright_gwlc/mass_grid_gp10.py
@@ -121,7 +121,10 @@ def run_EOS(EOS, m1, m2, thetas, type_set = 'None', N_EOS = 100, model_set = 'Bu
     if EOS == "gp":
         # read Phil + Reed's EOS files
         # eospostdat = np.genfromtxt("/home/philippe.landry/nseos/eos_post_PSRs+GW170817+J0030.csv",names=True,dtype=None,delimiter=",")
-        eospostdat = np.genfromtxt(f"{home_dir}/em-bright/ligo/em_bright/EOS_samples_unit_test/eos_post_PSRs+GW170817+J0030.csv",names=True,dtype=None,delimiter=",")
+        path_post = "/em-bright/ligo/em_bright/EOS_samples_unit_test/eos_post_PSRs+GW170817+J0030.csv"
+        path_post = os.path.join(home_dir, path_post)
+        eospostdat = np.genfromtxt(path_post, names=True, dtype=None, delimiter=",")
+        #eospostdat = np.genfromtxt(f"{home_dir}/em-bright/ligo/em_bright/EOS_samples_unit_test/eos_post_PSRs+GW170817+J0030.csv",names=True,dtype=None,delimiter=",")
         idxs = np.array(eospostdat["eos"])
         weights = np.array([np.exp(weight) for weight in eospostdat["logweight_total"]])
     elif EOS == "Sly":
@@ -163,7 +166,9 @@ def run_EOS(EOS, m1, m2, thetas, type_set = 'None', N_EOS = 100, model_set = 'Bu
             elif EOS == "gp":
                 while (lambda1 < 0.) or (lambda2 < 0.) or (mbns < 0.):
                     phasetr = 0
-                    eospath = f"{home_dir}/em-bright/ligo/em_bright/EOS_samples_unit_test/MACROdraw-1151%d-%d.csv" % (index, phasetr)
+                    eospath = "/em-bright/ligo/em_bright/EOS_samples_unit_test/MACROdraw-1151%d-%d.csv" % (index, phasetr)
+                    eospath = os.path.join(home_dir, eospath)
+                    # eospath = f"{home_dir}/em-bright/ligo/em_bright/EOS_samples_unit_test/MACROdraw-1151%d-%d.csv" % (index, phasetr)
                     # eospath = "/home/philippe.landry/nseos/eos/gp/mrgagn/DRAWmod1000-%06d/MACROdraw-%06d/MACROdraw-%06d-%d.csv" % (idxs[index]/1000, idxs[index], idxs[index], phasetr)
                     while os.path.isfile(eospath):
                         data_out = np.genfromtxt(eospath, names=True, delimiter=",")
